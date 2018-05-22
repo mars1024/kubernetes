c88118782dbcf57bda31dec40d6e4c3e10dc3ade
wojtekt <wojtekt@google.com>
2018-05-22 16:34:25 +0200

    Add coordination API group with Lease type

--- pkg/apis/coordination/validation/validation_test.go
+++ pkg/apis/coordination/validation/validation_test.go
@@ -82,23 +82,7 @@ func TestValidateLeaseSpecUpdate(t *testing.T) {
 		},
 	}
 	errs := ValidateLeaseUpdate(lease, oldLease)
-	if len(errs) != 3 {
+	if len(errs) != 2 {
 		t.Errorf("unexpected list of errors: %#v", errs.ToAggregate().Error())
-	}
-
-	validLeaseDuration := int32(10)
-	validLeaseTransitions := int32(20)
-	validLease := &coordination.Lease{
-		ObjectMeta: oldLease.ObjectMeta,
-		Spec: coordination.LeaseSpec{
-			HolderIdentity:       &holder,
-			LeaseDurationSeconds: &validLeaseDuration,
-			LeaseTransitions:     &validLeaseTransitions,
-		},
-	}
-	validLease.ObjectMeta.ResourceVersion = "2"
-	errs = ValidateLeaseUpdate(validLease, oldLease)
-	if len(errs) != 0 {
-		t.Errorf("unexpected list of errors for valid update: %#v", errs.ToAggregate().Error())
 	}
 }

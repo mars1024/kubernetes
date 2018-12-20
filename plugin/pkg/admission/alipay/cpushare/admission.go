a0b7d1d64e916b4ce170659fa550336e639a1e75
之芥 <zhijie.ww@antfin.com>
2018-12-20 14:56:01 +0800

    admission: add ant cpushare injection admission

---
+++ plugin/pkg/admission/alipay/cpushare/admission.go
@@ -0,0 +1,139 @@
+package cpushare
+
+import (
+	"io"
+
+	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apiserver/pkg/admission"
+
+	"k8s.io/kubernetes/pkg/apis/core"
+)
+
+const (
+	// PluginName is the name for current plugin, it should be unique among all plugins
+	PluginName = "AlipayCPUShareInject"
+
+	cpushareModeEnvName  = "ALIPAY_SIGMA_CPUMODE"
+	cpushareModeEnvValue = "cpushare"
+
+	cpushareVolumeName = "cpushare-volume"
+	cpusharePatchFile  = "/lib/libsysconf-alipay.so"
+)
+
+var (
+	_ admission.MutationInterface = &AlipayCPUShareAdmission{}
+)
+
+// Register is used to register current plugin to APIServer
+func Register(plugins *admission.Plugins) {
+	plugins.Register(PluginName, func(config io.Reader) (admission.Interface, error) {
+		return newAlipayCPUShareAdmission(), nil
+	})
+}
+
+// AlipayCPUShareAdmission is the main struct to inject cpushare related fields to pod.
+type AlipayCPUShareAdmission struct {
+	*admission.Handler
+}
+
+func newAlipayCPUShareAdmission() *AlipayCPUShareAdmission {
+	return &AlipayCPUShareAdmission{
+		Handler: admission.NewHandler(admission.Create),
+	}
+}
+
+// Admit makes an admission decision based on the request attributes.
+func (a *AlipayCPUShareAdmission) Admit(attr admission.Attributes) (err error) {
+	if shouldIgnore(attr) {
+		return nil
+	}
+
+	pod := attr.GetObject().(*core.Pod)
+	if err := a.injectCPUShare(pod); err != nil {
+		return errors.NewInternalError(err)
+	}
+
+	return nil
+}
+
+func hasEnv(envs []core.EnvVar, envName string) bool {
+	for _, env := range envs {
+		if env.Name == envName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasVolumeMount(mounts []core.VolumeMount, volumeName string) bool {
+	for _, m := range mounts {
+		if m.Name == volumeName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (a *AlipayCPUShareAdmission) injectCPUShare(pod *core.Pod) error {
+	// add volumes to pod.
+	// All pods should mount hostPath `/lib/libsysconf-alipay.so` to containers
+
+	hostPathFile := core.HostPathFile
+	pod.Spec.Volumes = append(pod.Spec.Volumes, core.Volume{
+		Name: cpushareVolumeName,
+		VolumeSource: core.VolumeSource{
+			HostPath: &core.HostPathVolumeSource{
+				Path: cpusharePatchFile,
+				Type: &hostPathFile,
+			},
+		},
+	})
+
+	for i := range pod.Spec.Containers {
+		// FIXME: this clearly adds env and volume mounts to all containers in the pod,
+		// should we skip some containers? such as sidecar container?
+
+		// add container env: cpusetmode=cpushare
+		if !hasEnv(pod.Spec.Containers[i].Env, cpushareModeEnvName) {
+			pod.Spec.Containers[i].Env = append(pod.Spec.Containers[i].Env, core.EnvVar{
+				Name:  cpushareModeEnvName,
+				Value: cpushareModeEnvValue,
+			})
+		}
+
+		// add volumeMounts
+		if !hasVolumeMount(pod.Spec.Containers[i].VolumeMounts, cpushareVolumeName) {
+			pod.Spec.Containers[i].VolumeMounts = append(pod.Spec.Containers[i].VolumeMounts, core.VolumeMount{
+				Name:      cpushareVolumeName,
+				MountPath: cpusharePatchFile,
+			})
+		}
+	}
+	return nil
+}
+
+func shouldIgnore(a admission.Attributes) bool {
+	resource := a.GetResource().GroupResource()
+	if resource != core.Resource("pods") {
+		return true
+	}
+
+	if a.GetSubresource() != "" {
+		return true
+	}
+
+	_, ok := a.GetObject().(*core.Pod)
+	if !ok {
+		glog.Errorf("expected pod but got %s", a.GetKind().Kind)
+		return true
+	}
+
+	if a.GetOperation() != admission.Create {
+		// only admit created pod
+		return true
+	}
+	return false
+}

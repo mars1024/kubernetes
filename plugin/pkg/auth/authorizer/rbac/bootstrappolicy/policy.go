0a88e20fc0fe0e43a2c1f47a3ae3d794d58d11d0
zibo.hzb <zibo.hzb@antfin.com>
2019-01-26 21:17:03 +0800

    [auth] add update/patch capacity to default system:node clusterrole

--- plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy.go
+++ plugin/pkg/auth/authorizer/rbac/bootstrappolicy/policy.go
@@ -117,7 +117,7 @@ func NodeRules() []rbacv1.PolicyRule {
 
 		// Needed for the node to create/delete mirror pods.
 		// Use the NodeRestriction admission plugin to limit a node to creating/deleting mirror pods bound to itself.
-		rbacv1helpers.NewRule("create", "delete").Groups(legacyGroup).Resources("pods").RuleOrDie(),
+		rbacv1helpers.NewRule("create", "delete", "patch").Groups(legacyGroup).Resources("pods").RuleOrDie(),
 		// Needed for the node to report status of pods it is running.
 		// Use the NodeRestriction admission plugin to limit a node to updating status of pods bound to itself.
 		rbacv1helpers.NewRule("update", "patch").Groups(legacyGroup).Resources("pods/status").RuleOrDie(),

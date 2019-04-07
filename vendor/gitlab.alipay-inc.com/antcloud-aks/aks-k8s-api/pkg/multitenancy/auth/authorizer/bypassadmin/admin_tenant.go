5b410dc596443ca6e1d07cf1809b6b436a60489d
yue9944882 <291271447@qq.com>
2019-04-07 23:15:03 +0800

    [vendor] bypass admin tenat authz

--- vendor/gitlab.alipay-inc.com/antcloud-aks/aks-k8s-api/pkg/multitenancy/auth/authorizer/bypassadmin/admin_tenant.go
+++ vendor/gitlab.alipay-inc.com/antcloud-aks/aks-k8s-api/pkg/multitenancy/auth/authorizer/bypassadmin/admin_tenant.go
@@ -3,6 +3,7 @@ package bypassadmin
 import (
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"gitlab.alipay-inc.com/antcloud-aks/aks-k8s-api/pkg/multitenancy/util"
+	"github.com/golang/glog"
 )
 
 type byPassAdminTenantAuthz struct{}
@@ -15,6 +16,14 @@ func NewByPassingAdminTenantAuthorizer() authorizer.Authorizer {
 
 func (byPassAdminTenantAuthz) Authorize(requestAttributes authorizer.Attributes) (authorizer.Decision, string, error) {
 	if util.IsMultiTenancyWiseAdmin(requestAttributes.GetUser().GetName()) {
+		return authorizer.DecisionAllow, "", nil
+	}
+	tenant, err := util.TransformTenantInfoFromUser(requestAttributes.GetUser())
+	if err != nil {
+		glog.Warning("fail to extract tenant info from user: %v", requestAttributes.GetUser())
+		return authorizer.DecisionNoOpinion, "", nil
+	}
+	if util.IsMultiTenancyWiseTenant(tenant) {
 		return authorizer.DecisionAllow, "", nil
 	}
 	return authorizer.DecisionNoOpinion, "", nil

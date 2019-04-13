8e7f819b733cc9def9826c595be0d8f3cdb22b82
zibo.hzb <zibo.hzb@antfin.com>
2019-04-13 12:53:38 +0800

    [scheduler] only update node cache when scheduling property changed

--- pkg/scheduler/core/scheduling_queue_mt.go
+++ pkg/scheduler/core/scheduling_queue_mt.go
@@ -3,9 +3,11 @@ package core
 import (
 	"fmt"
 
-	"gitlab.alipay-inc.com/antcloud-aks/aks-k8s-api/pkg/multitenancy/util"
+	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/tools/cache"
+
+	"gitlab.alipay-inc.com/antcloud-aks/aks-k8s-api/pkg/multitenancy/util"
 )
 
 func MultiTenancyKeyFuncWrapper(keyFunc KeyFunc) KeyFunc {
@@ -30,4 +32,19 @@ func MultiTenancyKeyFuncWrapper(keyFunc KeyFunc) KeyFunc {
 		}
 		return key, nil
 	}
+}
+
+func newUnschedulablePodsMapWithExtendKeyFunc() *UnschedulablePodsMap {
+	return &UnschedulablePodsMap{
+		pods:    make(map[string]*v1.Pod),
+		keyFunc: getPodFullName,
+	}
+}
+
+func getPodFullName(pod *v1.Pod) string {
+	tenantInfo, err := util.TransformTenantInfoFromAnnotations(pod.Annotations)
+	if err == nil {
+		return util.TransformTenantInfoToJointString(tenantInfo, "/") + "/" + pod.Namespace + "/" + pod.Name
+	}
+	return pod.Namespace + "/" + pod.Name
 }

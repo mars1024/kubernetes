bfcd3b79f330da4cfaf3bcddd6c089254c86f44c
Di Xu <stephen.xd@antfin.com>
2019-05-06 20:33:02 +0800

    allow empty tenant info when creating namespaced scoped resources

--- plugin/pkg/admission/privatecloud/admission.go
+++ plugin/pkg/admission/privatecloud/admission.go
@@ -89,14 +89,15 @@ func (plugin *privateCloudPlugin) Admit(a admission.Attributes) error {
 
 	namespace, err := plugin.lister.Get(a.GetNamespace())
 	if err != nil {
-		return fmt.Errorf("getting namespace %q failed: %v", a.GetNamespace(), err)
+		glog.Warningf("getting namespace %q failed: %v", a.GetNamespace(), err)
+		return nil
 	}
 
 	glog.V(5).Infof("Trying to get tenant info from namespace %q...", namespace.Name)
 	tenant, err := getTenantInfoFromLabelsOrAnnotations(*namespace)
 	if err != nil {
-		glog.Errorf("Failed to get tenant info from namespace %q with error: %v", namespace.Name, err)
-		return err
+		glog.Warningf("Failed to get tenant info from namespace %q with error: %v", namespace.Name, err)
+		return nil
 	}
 
 	glog.V(5).Infof("Trying to inject tenant info %+v to %s/%s/%s labels", tenant, a.GetNamespace(), a.GetResource(), a.GetName())

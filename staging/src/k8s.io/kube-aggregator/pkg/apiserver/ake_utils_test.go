be91dbb782cd9c04a66682b2a0bc18832470039b
Di Xu <stephen.xd@antfin.com>
2019-05-08 17:10:11 +0800

    allowing aa server with http scheme

--- staging/src/k8s.io/kube-aggregator/pkg/apiserver/ake_utils_test.go
+++ staging/src/k8s.io/kube-aggregator/pkg/apiserver/ake_utils_test.go
@@ -4,37 +4,48 @@ import (
 	"testing"
 )
 
-func TestParse(t *testing.T) {
+func TestParseAKEApiService(t *testing.T) {
 	testCases := []struct {
 		serverAddr       string
+		expectedScheme   string
 		expectedHost     string
 		expectedHostName string
 		expectedErr      bool
 	}{
 		{
 			serverAddr:       "https://test.me:80",
+			expectedScheme:   "https",
 			expectedHost:     "test.me:80",
 			expectedHostName: "test.me",
 		},
 		{
 			serverAddr:       "https://test.me:80/",
+			expectedScheme:   "https",
 			expectedHost:     "test.me:80",
 			expectedHostName: "test.me",
 		},
 		{
 			serverAddr:       "https://test.me/",
+			expectedScheme:   "https",
 			expectedHost:     "test.me",
 			expectedHostName: "test.me",
 		},
 		{
 			serverAddr:       "test.me:8080",
+			expectedScheme:   "https",
+			expectedHost:     "test.me:8080",
+			expectedHostName: "test.me",
+		},
+		{
+			serverAddr:       "http://test.me:8080",
+			expectedScheme:   "http",
 			expectedHost:     "test.me:8080",
 			expectedHostName: "test.me",
 		},
 	}
 
 	for idx, tc := range testCases {
-		gotHost, gotHostName, err := parseAKEApiService(tc.serverAddr)
+		gotScheme, gotHost, gotHostName, err := parseAKEApiService(tc.serverAddr)
 		if err != nil {
 			if !tc.expectedErr {
 				t.Errorf("[%d] unexpected error: %v", idx, err)
@@ -43,6 +54,9 @@ func TestParse(t *testing.T) {
 			if tc.expectedErr {
 				t.Errorf("[%d] expected error, but got nil", idx)
 			} else {
+				if gotScheme != tc.expectedScheme {
+					t.Errorf("[%d] scheme not equal: expected %q, but got %q", idx, tc.expectedScheme, gotScheme)
+				}
 				if gotHost != tc.expectedHost {
 					t.Errorf("[%d] host not equal: expected %q, but got %q", idx, tc.expectedHost, gotHost)
 				}
